ee6f5068c0bfacc642b572640382c8b6be42c1a4
aomine001 <77345354+aomine001@users.noreply.github.com>
2023-08-22 12:06:02 +0000

    Meow

--- main.cpp
+++ main.cpp
@@ -1,26 +1,65 @@
 #include <iostream>
-#include <fstream>
-#include <cassert>
+#include <stdlib.h>
+#include <cstring>
 
-int memory_size = 0x10000; // 
+int memory_size = 0x10000;
 uint8_t* memory = new uint8_t[memory_size];
 
-void load_sector(std::fstream file, unsigned long baseSector, unsigned long sectorCount, unsigned long loadAddress)
+long pc, sp;
+long a, b, c, d;
+
+long* gp_Registers[] = {&a, &b, &c, &d, &sp};
+
+bool f_Halted = false;
+
+uint8_t fetch_byte()
 {
-    if (!file.is_open())
-    {
-        std::cout << "load_sector(): File isn't open.\n";
-
-        return;
-    }
-
-    file.seekg(baseSector * 512);
-    file.read((char*)memory[loadAddress], sectorCount * 512);
+    if (pc >= memory_size) return 0;
+    uint8_t v = memory[pc];
+    pc ++;
+    return v;
 }
+
+#define NOP 0
+#define MOVE_BYTE_TO_REG 1
+#define HALT 2
 
 int main(int argc, char** argv)
 {
-    std::fstream file("test.txt", std::ios::binary);
+    memset(memory, 0, memory_size);
+
+    pc = 0;
+    a = b = c = d = sp = 0;
+
+    memory[0] = MOVE_BYTE_TO_REG;
+    memory[1] = 0;
+    memory[2] = 'i';
+    memory[3] = HALT;
+
+    uint8_t tr1, tr2;
+    while (!f_Halted)
+    {
+        uint8_t opcode = fetch_byte();
+
+        switch (opcode){
+            case NOP:
+                break;
+            case MOVE_BYTE_TO_REG:
+                tr1 = fetch_byte();
+                tr2 = fetch_byte();
+
+                *gp_Registers[tr1] = tr2;
+                break;
+            case HALT:
+                f_Halted = true;
+                break;
+            default:
+                printf("Unknown instruction 0x%X at 0x%lX", opcode, pc);
+            break;
+        }
+    }
+
+    std::cout << a;
 
     delete[] memory;
     return 0;
